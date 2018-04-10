17ec87004577eec23d22ac0c86d2fa45ef350e64
Jules Guesnon <JulesGuesnon@users.noreply.github.com>
2018-04-10 15:19:44 +0200

    Add the textures
    textures

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import css from './css/app.scss'
 import Character from './js/class/characterClass.js'
 import Animate from './js/functions/animate.js'
+import Level from './js/class/levelClass.js'
 
 let canvas  = document.querySelector('canvas')
 
@@ -12,11 +13,17 @@ canvas.height = 300
 
 self.goku = new Character('Goku', 'sprites/goku.png',51,500,500,ctx)
 self.nyancat = new Character('nyanCat', 'sprites/NYANcat-good.png',75,500,500,ctx)
-self.rayquaza = new Character('Rayquaza', 'sprites/RAYQUAZA_good.png',72.74,200,200,ctx)
+self.rayquaza = new Character('rayquaza', 'sprites/RAYQUAZA_good.png',72.67,500,500,ctx)
 
-function tamere() {
-    requestAnimationFrame(tamere)
-    Animate(goku)
+self.desert = new Level(0,'ressources/background/desert.png','ressources/floor/sol_de_terre.png',canvas.width,canvas.height,ctx)
+
+self.desert.block.onload = function () {
+    ctx.drawImage(desert.block,0,0,desert.block.width,desert.block.height,0,canvas.height-100,400,100)
 }
 
-tamere()
+// function tamere() {
+//     requestAnimationFrame(tamere)
+//     Animate(goku, desert)
+// }
+
+// tamere()
