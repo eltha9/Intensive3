b74807fed3383958b58cc9e945e1e6dce2e1c574
Jules Guesnon <JulesGuesnon@users.noreply.github.com>
2018-04-10 12:28:47 +0200

    Set up character class
    Put the functions in the class

--- index.js
+++ index.js
@@ -1,36 +1,14 @@
 import css from './css/app.scss'
+import Character from './js/class/characterClass.js'
 
 let canvas  = document.querySelector('canvas')
+
 let ctx = canvas.getContext('2d')
 ctx.imageSmoothingEnabled = false
-
-self.img = document.createElement('img')
-self.img.src = "sprites/RAYQUAZA_good.png"
-self.img.onload = function () {
-    animate()
-}
 
 canvas.width = 700
 canvas.height = 300
 
-let step = 0
-let steps = 0
-
-
-function animate() {
-    requestAnimationFrame(animate)
-    draw(img.height,Math.floor(steps))
-    update()
-}
-
-function draw(height,step) {
-    ctx.clearRect(0,0,innerWidth,innerHeight)
-    ctx.drawImage(self.img,73*step,0,73,self.img.height,0,0,73,self.img.height)
-}
-
-function update() {
-    steps += 0.15
-       if (steps > 8) {
-        steps = 0
-    }
-}
+self.goku = new Character('Goku', 'sprites/goku.png',51,500,500,ctx)
+console.log(goku)
+self.goku.animate()
--- js/class/characterClass.js
+++ js/class/characterClass.js
@@ -5,17 +5,41 @@
  * @param  {number} costPoints 
  */
 
-class Character {
-    constructor(name,sprite,costCoins,costPoints) {
+export default class Character {
+    constructor(name,sprite,spriteWidth,costCoins,costPoints,ctx) {
 
         this.name = name
 
-        this.sprite = sprite
+        this.sprite = new Image()
+        this.sprite.src = sprite
+        this.spriteWidth = spriteWidth
+
+        this.ctx = ctx
 
         this.costCoins = costCoins
         this.costPoints = costPoints
 
         this.selectionWidth = 75
-        this.height = sprite.height
+        this.height = this.sprite.height
+
+        this.steps = 0
+    }
+
+    animate() {
+        // requestAnimationFrame(this.animate())
+        this.draw(this.height,Math.floor(this.steps))
+        this.update()
+    }
+
+    draw(height,step) {
+        this.ctx.clearRect(0,0,innerWidth,innerHeight)
+        this.ctx.drawImage(this.sprite,this.spriteWidth*step,0,this.spriteWidth,this.height,20,20,this.spriteWidth,this.height)
+    }
+
+    update() {
+        this.steps += 0.15
+           if (this.steps > 7) {
+            this.steps = 0
+        }
     }
 }
